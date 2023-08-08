1889f62b300f0a018327f3ef589cbf115039cde5
touraymk <131713898+touraymk@users.noreply.github.com>
2023-08-08 17:30:58 +0200

    Update App.ja
    Added repository link.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+      <a href="https://github.com/touraymk/hw-react-weather-app" rel="noreferrer">Open-sourced</a> by Mariama Touray.
+      </p>
     </div>
   );
 }
