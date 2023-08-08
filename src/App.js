d8e12a0f3e37c25f862c718988d0dee7e49b30d0
touraymk <131713898+touraymk@users.noreply.github.com>
2023-08-08 16:57:31 +0200

    Created SearchEngine.js and linked it to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import SearchEngine from "./SearchEngine";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <SearchEngine />
     </div>
   );
 }
---
+++ src/SearchEngine.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchEngine() {
+    return <h2>Search Engine</h2>
+}
